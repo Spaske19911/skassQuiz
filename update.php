fc2b0a4fc2937fe05e382907704b260f087dc8a0
Spaske19911 <spaske19911@gmail.com>
2016-11-10 13:03:08 +0100

    Update update.php

--- update.php
+++ update.php
@@ -1,24 +1,22 @@
-<html>
-<head>
-
-</head>
-<body>
     
-    
-     <?php
-
+ <?php
     $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname = "android_quiz";
-
     $conn = new mysqli($servername, $username, $password, $dbname);
-
     if ($conn->connect_error){
         die("Konekcija nije uspela:".$conn->connect_error);
     }
    
-   
+    $pitanje = ""; 
+    $odgovor1 = "";
+    $odgovor2 = "";
+    $odgovor3 = "";
+    $odgovor4 = "";
+    $tacan =  "";
+    
+    
     $pitanje = $_REQUEST["pitanje"];
     $odgovor1 =  $_REQUEST["odg1"];
     $odgovor2 =  $_REQUEST["odg2"];
@@ -28,7 +26,6 @@
     
         $zamena = "INSERT INTO pitanja (pitanje, odgovori1, odgovori2, odgovori3, odgovori4, tacan_odgovor)
         VALUES ( '$pitanje', '$odgovor1', '$odgovor2','$odgovor3','$odgovor4', '$tacan')";
-
             if ($conn->query($zamena) === TRUE) {
             echo "New record created successfully";
         } else {
@@ -36,7 +33,6 @@
         }
     
     
-
         $sql_read =  "SELECT pitanje, odgovori1,odgovori2,odgovori3,odgovori4, tacan_odgovor FROM pitanja";
             $result = $conn->query($sql_read);
             if ($result->num_rows > 0) {
@@ -47,25 +43,6 @@
         } else {
             echo "0 results";
         }
-
     
-
 $conn->close();
 ?>
-
-<form action="update.php" method="post" >
-Pitanje <input name="pitanje" type="text"> <br>
-Odgovor1 <input name="odg1" type="text"><br>
-Odgovor2 <input name="odg2" type="text"> <br>
-Odgovor3 <input name="odg3" type="text"> <br>
-Odgovor4 <input name="odg4" type="text"><br>
-Tacan odgovor <input name="tac" type="text"><br>
-<input type="submit">
-
-</form>
-    
-
-    
-    
-</body>
-</html>
